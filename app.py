27d4594e5f7eeefa2ecd380179a40a2287e6dd3a
Bongumusa Joseph Hlongwane <94066555+Joemusa@users.noreply.github.com>
2026-02-06 14:36:18 +0200

    Fix webhook URL environment variable loading

--- app.py
+++ app.py
@@ -4,13 +4,12 @@ import os
 
 # -----------------------------
 # CONFIG
-st.write("Webhook URL loaded:", MAKE_WEBHOOK_URL)
 
 
 # -----------------------------
-MAKE_WEBHOOK_URL = os.getenv("https://hook.eu2.make.com/lbeu75blbrpwphhvm8un8pe36mam2xp3")
+MAKE_WEBHOOK_URL = os.getenv("MAKE_WEBHOOK_URL")
 
-
+t.write("Webhook URL loaded:", MAKE_WEBHOOK_URL)
 
 st.set_page_config(page_title="Analytics Calculation Assistant")
 
